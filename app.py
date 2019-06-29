d9983e44db1520c8d778ec62fe390411e965c235
Michal Madera <michal.jan.madera@gmail.com>
2019-06-29 18:27:30 +0200

    adding index.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return 'Welcome to the main page'
+    return index.html
 @app.route('/author')
 def about_author():
     return 'This webpage have been created by Michal Madera'
