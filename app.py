498bbd44c47a97d96654e57656404cabeb79eb70
Michal Madera <michal.jan.madera@gmail.com>
2019-06-29 18:19:33 +0200

    main page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home_page():
     return 'Welcome to the main page'
 @app.route('/author')
-def about_author:
+def about_author():
     return 'This webpage have been created by Michal Madera'
 
 if __name__ == "__main__":
